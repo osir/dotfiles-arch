44f56d9b65fada4dda5c92a5a44ae574890faab2
Simone Viscardi <srgv61@gmail.com>
2016-05-26 08:01:16 +0200

    Added tree function

--- home/.oh-my-zsh/custom/functions.zsh
+++ home/.oh-my-zsh/custom/functions.zsh
@@ -64,3 +64,24 @@ psfind() {
     ps -A \
         | grep -e "${regex}"
 }
+
+# Shows a graphical representation of the current sub-directories
+# Params: [base dir]
+tree() {
+    echo
+    if [ "$1" != "" ]  #if parameter exists, use as base folder
+    then cd "$1"
+    fi
+    pwd
+    ls -R | grep ":$" |   \
+    sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'
+    # 1st sed: remove colons
+    # 2nd sed: replace higher level folder names with dashes
+    # 3rd sed: indent graph three spaces
+    # 4th sed: replace first dash with a vertical bar
+
+    if [ $(ls -F -1 | grep "/" | wc -l) = 0 ]   # check if no folders
+    then echo "   -> no sub-directories"
+    fi
+    echo
+}
