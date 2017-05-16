828f26139fea6ee772f0c5ae76a656ed8d0698fa
osiris <srgv61@gmail,com>
2017-05-16 20:25:26 +0200

    Renamed files to not be hidden files in the repository

--- setupconfig.sh
+++ setupconfig.sh
@@ -2,17 +2,17 @@
 
 dotfiles_dir="$HOME/repos/dotfiles"
 declare -A links=(
-  [home/.config/compton.conf]=~/.config/compton.conf
-  [home/.config/dunst]=~/.config/dunst
-  [home/.config/i3]=~/.config/i3
-  [home/.config/lightdm]=~/.config/lightdm
-  [home/.oh-my-zsh/custom/aliases.zsh]=~/.oh-my-zsh/custom/aliases.zsh
-  [home/.oh-my-zsh/custom/functions.zsh]=~/.oh-my-zsh/custom/functions.zsh
-  [home/.tp]=~/.tp
-  [home/.vimrc]=~/.vimrc
-  [home/.vim]=~/.vim
-  [home/.xprofile]=~/.xprofile
-  [home/.Xresources]=~/.Xresources
-  [home/.zshrc]=~/.zshrc
+  [home/config/compton.conf]=~/.config/compton.conf
+  [home/config/dunst]=~/.config/dunst
+  [home/config/i3]=~/.config/i3
+  [home/config/lightdm]=~/.config/lightdm
+  [home/oh-my-zsh/custom/aliases.zsh]=~/.oh-my-zsh/custom/aliases.zsh
+  [home/oh-my-zsh/custom/functions.zsh]=~/.oh-my-zsh/custom/functions.zsh
+  [home/tp]=~/.tp
+  [home/vimrc]=~/.vimrc
+  [home/vim]=~/.vim
+  [home/xprofile]=~/.xprofile
+  [home/Xresources]=~/.Xresources
+  [home/zshrc]=~/.zshrc
 )
 
