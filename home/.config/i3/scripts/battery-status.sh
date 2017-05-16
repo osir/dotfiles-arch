0d2634241b050feee755ef991e774c76295ee7ed
osiris <srgv61@gmail,com>
2017-05-16 08:26:24 +0200

    Removed Caps Icon, added alert for low battery

--- home/.config/i3/scripts/battery-status.sh
+++ home/.config/i3/scripts/battery-status.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-b="$(cat /sys/class/power_supply/BAT0/capacity)%"
+b="$(cat /sys/class/power_supply/BAT0/capacity)"
+if [ "$b" -lt 15 ]; then
+    notify-send --urgency=critical --expire-time=60000 --app-name=BATTERY "Battery is low: $b%"
+fi
 if [ "$(cat /sys/class/power_supply/BAT0/status)" == 'Charging' ]; then
     b="⁺${b}"
 fi
-printf '%s' "$b"
+printf '%s%%' "$b"
