66aade53fcc67e040cf63bf7dcf0ff02b7ecb117
osiris <srgv61@gmail,com>
2017-05-16 20:32:51 +0200

    Added relink option to setup, cleaned up code

--- setup.sh
+++ setup.sh
@@ -9,36 +9,53 @@ main() {
 
     case "$mode" in
         'link'|'Link')
-            echo 'Creating links:'
-            for i in "${!links[@]}"; do
-                local src="$i"
-                local dest="${links[$i]}"
-                if [ -L "$dest" ]; then
-                    printf '[INFO] Skiping existing link: %s\n' "$dest"
-                    continue
-                fi
-                printf '[INFO] Linking: %s => %s\n' "$src" "$dest"
-                ln --symbolic "$PWD/$src" "$dest"
-            done
+            setup_links
             ;;
         'clean'|'Clean')
-            echo 'Removing links:'
-            for i in "${!links[@]}"; do
-                local link="${links[$i]}"
-                printf '[INFO] Removing: %s\n' "$link"
-                if [ -L "$link" ]; then
-                    rm --verbose "$link"
-                fi
-            done
+            setup_clean
+            ;;
+        'relink'|'Relink')
+            setup_clean
+            setup_links
             ;;
         *)
-            printf 'Unknown option "%s".\n' "$mode"
-            printf 'Usage: setup.sh {link|clean}\n'
-            printf '\nOptions:\n'
-            printf '\t%s\t%s\n' 'link:'  'Links files to installation destinations'
-            printf '\t%s\t%s\n' 'clean:' 'Removes created files'
+            printf 'Unknown option "%s".\n\n' "$mode"
+            print_help
             ;;
     esac
+}
+
+setup_links() {
+    echo 'Creating links:'
+    for i in "${!links[@]}"; do
+        local src="$i"
+        local dest="${links[$i]}"
+        if [ -L "$dest" ]; then
+            printf '[INFO] Skiping existing link: %s\n' "$dest"
+            continue
+        fi
+        printf '[INFO] Linking: %s => %s\n' "$src" "$dest"
+        ln --symbolic "$PWD/$src" "$dest"
+    done
+}
+
+setup_clean() {
+    echo 'Removing links:'
+    for i in "${!links[@]}"; do
+        local link="${links[$i]}"
+        printf '[INFO] Removing: %s\n' "$link"
+        if [ -L "$link" ]; then
+            rm --verbose "$link"
+        fi
+    done
+}
+
+print_help() {
+    printf 'Usage: setup.sh {link|clean|relink}\n'
+    printf '\nOptions:\n'
+    printf '\t%s\t%s\n' 'link:'   'Links files to installation destinations'
+    printf '\t%s\t%s\n' 'clean:'  'Removes created files'
+    printf '\t%s\t%s\n' 'relink:' 'Removes created files and creates new links'
 }
 
 main $*
