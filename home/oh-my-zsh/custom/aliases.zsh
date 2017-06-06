722bbacd129228182d43620a8319c997ab5f3789
osiris <srgv61@gmail,com>
2017-06-06 13:34:28 +0200

    Cleaned up aliases

--- home/oh-my-zsh/custom/aliases.zsh
+++ home/oh-my-zsh/custom/aliases.zsh
@@ -1,32 +1,19 @@
-# general
-# -------
+# ls
 alias ll="\ls -lAh --color=auto --group-directories-first"
 alias ls="\ls -lh --color=auto --group-directories-first"
-alias l="ll"
+alias l='ll'
 
-alias vi="vim"
-alias shutdown="sudo \shutdown -h now"
-
-alias count="wc -l"
-alias battery-status='upower -i /org/freedesktop/UPower/devices/battery_BAT0| grep -E "state|time\ to\ full|percentage"'
-alias bat='cat /sys/class/power_supply/BAT0/capacity'
-
-alias terminal="urxvtc -fn 'xft:Ubuntu Mono derivative Powerline:size=12:style=regular'"
-
+# pacman
 alias syyu='sudo pacman -Syyu'
 alias s='sudo pacman -S'
-
-# services / daemons
-# ------------------
 
 # SSH
 alias sshoff='sudo systemctl stop sshd.service'
 alias sshon='sudo systemctl start sshd.service'
 
 # other
-# -----
-alias öö='sl'
-alias ös='sl'
-
 alias fm='vifm . ~'
+alias vi='vim'
+alias terminal="urxvtc -fn 'xft:Ubuntu Mono derivative Powerline:size=12:style=regular'"
+alias battery-status='upower -i /org/freedesktop/UPower/devices/battery_BAT0| grep -E "state|time\ to\ full|percentage"'
 
