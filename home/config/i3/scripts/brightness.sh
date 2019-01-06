3db33f7f91a9916379a4e8aef0e86755bf09adc7
os <gh@svis.ch>
2019-01-06 05:52:42 +0100

    Fixed ultra low brighness mode

--- home/config/i3/scripts/brightness.sh
+++ home/config/i3/scripts/brightness.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
 
 case $BLOCK_BUTTON in
-    1)
+    1)  # LMB
         notify-send -t 1000 'Redshift' 'Disabling blue light filter...'
         redshift -rPO 6500K > /dev/null
         ;;
-    3)
+    2)  # MMB
+        notify-send -t 1000 'Brightness' 'Enabling ultra low brightness...'
+        light -S 0.1
         notify-send -t 1000 'Redshift' 'Enabling blue light filter...'
         redshift -rPO 4500K > /dev/null
         ;;
-    4)
+    3)  # RMB
+        notify-send -t 1000 'Redshift' 'Enabling blue light filter...'
+        redshift -rPO 4500K > /dev/null
+        ;;
+    4)  # UP
         light -A 5
         ;;
-    5)
+    5)  # DOWN
         light -U 5
         ;;
 esac
 
 c=$(light -G | cut -d . -f 1)
-
 printf '☀%s\n' "$c"
 
