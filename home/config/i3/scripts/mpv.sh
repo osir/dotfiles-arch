cbab23d04a1e5c12a202cb905d10efe916546dca
os <git@osiris.moe>
2024-01-15 21:57:49 +0100

    Clearing all configuration for change to Fedora/Sway

--- home/config/i3/scripts/mpv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-chosen=$(rofi -dmenu -p video -i -l 0)
-
-[ "$chosen" != "" ] || exit
-
-mpv "$chosen"
-
--- home/config/i3/scripts/wschange.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-choices=$(echo "$1" | tr ',' '\n')
-
-i3-msg workspace "$(echo "$choices" | rofi -dmenu -p switch -i)"
-
